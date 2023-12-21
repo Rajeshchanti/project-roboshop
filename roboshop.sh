02defee238d4ec7e21f48e271e88d454bfad61bc
rajesh chanti <Rajeshchanti>
2023-12-21 11:46:40 +0530

    ec2 creation using shell-script

--- roboshop.sh
+++ roboshop.sh
@@ -13,7 +13,7 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids   $SG_ID --tag-specifications "resourceType=Instance, Tags= [{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=Instance,Tags=[{Key=Name, Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $PRIVATE_IP"
 
     #create R53 record, make sure you delete existing record
